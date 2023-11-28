59b5f36d569af680742e10e8ca599920c4596a0a
NailSacul <ianlucaschaves@gmail.com>
2023-11-28 02:22:08 -0300

    Project created

---
+++ src/main/java/com/naisacul/challengebackendpicpay/ChallengeBackendPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengebackendpicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendPicpayApplication.class, args);
+	}
+
+}
